d5b21f550effe169c5df7a7a9224badc8cd07446
jeongyounghun <dudgns3681@naver.com>
2023-08-28 13:47:37 +0900

    자바의 정석 교재 공부

---
+++ src/Chapter06/Pr/Exercise6_18.java
@@ -0,0 +1,20 @@
+package Chapter06.Pr;
+
+public class Exercise6_18 {
+    public static void main(String[] args) {
+        String str = "123";
+        System.out.println(str + "는 숫자입니까? " + isNumber(str));
+        str = "1234o";
+        System.out.println(str + "는 숫자입니까? " + isNumber(str));
+    }
+
+    private static boolean isNumber(String str) {
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if (c < '0' || c > '9'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
